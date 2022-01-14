6deaab134cc4dcc388ab7c73ec7ed90b7739acb6
Ritchie Brannan <ritchie@morphicvoid.com>
2022-01-14 14:01:48 +0000

    Update in prep for new term

---
+++ UnrealProjects/NUA 4.25/Source/NUA/NUAGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NUAGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANUAGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANUAGameMode();
+};
+
+
+
